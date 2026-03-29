dc566062778fde16f351214819916210620720f3
Ryan Ribeiro <ryanribeirocontato@outlook.com>
2026-03-29 02:41:19 -0300

    setting up git

--- src/__main__.py
+++ src/__main__.py
@@ -6,6 +6,7 @@ import scanner
 
 parser = libs.argparse.ArgumentParser()
 parser.add_argument('-r', '--range', help='ip range you wanna scan for hosts in CIDR notation. (Example: 192.169.0.0/24)')
+parser.add_argument('-s', '--sniff', action='store_true' ,help="'sniff the interface you're in")
 args = parser.parse_args()
 
 if __name__ == '__main__':
@@ -13,3 +14,30 @@ if __name__ == '__main__':
         print(f'[/] Network range scanner selected [/]')
         devices = scanner.scanner(args.range)
         make_table.make_device_table(devices)
+    elif args.sniff:
+
+        iface = None
+        bpf_filter = None
+        count = None
+        timeout = None
+        write_pcap = False
+        
+        try:
+            print('[/] Network sniffer selected [/]')
+            while True:
+                inp = input('[?] Do you wish to setup parameters for the sniffer? [y/n]')
+                res = inp.lower()
+        
+                if res == 'y':
+                    pass
+                elif res == 'n':
+                    break
+                else:
+                    print('[!] Invalid response [!]')
+            clean_sniffing.sniffing(iface=iface,
+                                    filter=bpf_filter,
+                                    count=count,
+                                    timeout=timeout
+                                    )
+        except Exception as e:
+            print(f'[!] {e} [!]')
--- src/clean_sniffing.py
+++ src/clean_sniffing.py
@@ -2,3 +2,21 @@ import libs
 
 def clean_sniffing():
     pass
+def packet_tracer(packet):
+    if libs.scapy.IP in packet:
+        print(f'From {packet[libs.scapy.IP].src} to {packet[libs.scapy.IP].dst}')
+
+def sniffing(iface=None, filter=None, count=None, timeout=None):
+    kwargs = {
+            "prn": packet_tracer
+            }
+    if iface:
+        kwargs["iface"] = iface
+    if filter:
+        kwargs["filter"] = filter
+    if count is not None:
+        kwargs["count"] = count
+    if timeout is not None:
+        kwargs["timeout"] = timeout
+    
+    libs.scapy.sniff(**kwargs)
