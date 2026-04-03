7cb22ed496c7dc9a95a016bb75d604d6c20b6ba3
Ryan Ribeiro <ryanribeirocontato@outlook.com>
2026-04-02 21:32:07 -0300

    making the sniffer more bonkers

--- src/clean_sniffing.py
+++ src/clean_sniffing.py
@@ -1,12 +1,41 @@
 import libs
 
-def clean_sniffing():
-    pass
 def packet_tracer(packet):
+    proto = None
+    mac = None
+   
+    ip_flags = packet[libs.scapy.IP].flags if packet.haslayer(libs.scapy.IP) else None
+    
+    tcp_flags = packet[libs.scapy.TCP].flags if packet.haslayer(libs.scapy.TCP) else None
+    
     if libs.scapy.IP in packet:
-        print(f'From {packet[libs.scapy.IP].src} to {packet[libs.scapy.IP].dst}')
+        print(f"From {libs.ANSI['GREEN']}{packet[libs.scapy.IP].src}{libs.ANSI['END']} to {libs.ANSI['GREEN']}{packet[libs.scapy.IP].dst}{libs.ANSI['END']}", end="")
 
-def sniffing(iface=None, filter=None, count=None, timeout=None):
+        if packet.haslayer(libs.scapy.TCP):
+            proto = 'TCP'
+        elif packet.haslayer(libs.scapy.UDP):
+            proto = 'UDP'
+        elif packet.haslayer(libs.scapy.ICMP):
+            proto = 'ICMP'
+        else:
+            proto = 'OTHER'
+        
+        print(f' - {proto}', end='')
+    
+        if packet.haslayer(libs.scapy.Ether):
+            mac = packet[libs.scapy.Ether].src
+
+        print(f' - {mac}', end='')
+        
+        if proto == 'TCP':
+            print(f' - TCP flags: {tcp_flags}', end='')
+        
+        if ip_flags != None:
+            print(f' - IP flags: {ip_flags}')
+
+
+
+def sniffing(iface=None, filter=None, count=None, timeout=None, write=False):
     kwargs = {
             "prn": packet_tracer
             }
--- src/libs.py
+++ src/libs.py
@@ -5,3 +5,17 @@ import scapy.all as scapy
 import sys
 import pandas
 from time import sleep
+
+ANSI = {
+        'RED' : '\033[;31m',
+        'GREEN': '\033[;32m',
+        'BLUE': '\033[;34m',
+        'PURPLE': '\033[0;35m',
+        'LIGHT_RED': '\033[1;31m',
+        'LIGHT_GREEN': '\033[1;32m',
+        'LIGHT_BLUE': '\033[1;34m',
+        'YELLOW': '\033[1;33m',
+        'BOLD': '\033[1m',
+        'END': '\033[0m',
+        }
+
